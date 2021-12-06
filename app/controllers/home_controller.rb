b4e451735b8c412191284d975d45f5e3df795b72
Dmitry <dimsacom@gmail.com>
2021-12-07 01:06:56 +0300

    completion

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'sequence'
+
 # home controller
 class HomeController < ApplicationController
   skip_before_action :verify_authenticity_token
@@ -12,28 +14,7 @@ class HomeController < ApplicationController
     end
     p arr_str
     arr = arr_str.split(' ').map(&:to_i)
-    distances = find_distances(arr)
+    distances = Sequence.find_distances(arr)
     render json: { distances: distances, maxDistance: distances.max { |a, b| a.length <=> b.length } }
   end
-
-  def find_distances(arr)
-    arr
-      .map { |i| po5?(i) ? i : 0 }
-      .slice_when { |i, j| po5?(i) != po5?(j) }
-      .to_a
-      .reject { |i| i.include? 0 }
-  end
-
-  def po5?(num)
-    return false if num <= 0
-
-    while num != 1
-      return false unless (num % 5).zero?
-
-      num /= 5
-    end
-    true
-  end
-
-  helper_method :find_distances, :po5?
 end
